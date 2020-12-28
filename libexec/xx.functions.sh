0d42fac8a9aa2ec82fe2404a17273b283a1b14c5
Stephen Rogers <inbox.c7r@steve-rogers.com>
2020-12-28 10:14:23 -0500

    Pull in relevant updates from `snippets--sh`

--- libexec/xx.functions.sh
+++ libexec/xx.functions.sh
@@ -11,37 +11,39 @@ xx_debug_p=
 ##
 
 function xx() { # ...
+
+	xx_lod "" "$@"
+}
+
+function xx_lod() { # this_lod ...
 ##
-## Execute specified command as if `set -x` is in effect.
+## Execute the specified command as if `set -x` could automatically go into
+## effect at a specified level of detail.
+##
+## More precisely, environment variable `XX_LOD_THRESHOLD` governs whether
+## the equivalent of `set -x` is in effect. It will be in effect only when
+## `${XX_LOD_THRESHOLD}` is at least `${this_lod}`.
 ## 
-## Net result: the command is first echoed to stderr with a suitable
-## prefix, and then it is invoked.
+## Net result: when `${XX_LOD_THRESHOLD}` is at least `${this_lod}`, the
+## command is first echoed to stderr with a suitable prefix. Then,
+## regardless of `XX_LOD_THRESHOLD`, the command is invoked.
+## 
+## As for `set -x` itself, the prefix is governed by `PS4` (default `'+'`).
+## 
+## Note that both `XX_LOD_THRESHOLD` and `this_lod` default to 1. So, if
+## you want the `set -x` effect at the normal level of detail, you're all
+## set; if you want to disable the effect entirely, set `XX_LOD_THRESHOLD`
+## to 0; and if you want it only at higher levels of detail, set it to the
+## desired threshold.
 ##
-	echo 1>&2 "${PS4:-+}" "$@"
+	local default_lod="1" ; local this_lod="${1:-${default_lod:?}}" ; shift 1
+
+	if [ "${XX_LOD_THRESHOLD:-${default_lod:?}}" -ge "${this_lod:?}" ] ; then
+
+		echo 1>&2 "${PS4:-+}" "$@"
+	fi
 
 	"$@"
 }
 
-function xx_env() { # ...
 ##
-## Apply `/usr/bin/env` to the specified command as if `set -x` is in effect.
-## 
-## Net result: the command is first echoed to stderr with a suitable
-## prefix, and then it is invoked via `/usr/bin/env`.
-##
-	echo 1>&2 "${PS4:-+}" /usr/bin/env "$@"
-
-	/usr/bin/env "$@"
-}
-
-function xx_eval() { # ...
-##
-## Apply `eval` to the specified command as if `set -x` is in effect.
-## 
-## Net result: the command is first echoed to stderr with a suitable
-## prefix, and then it is eval'ed.
-##
-	echo 1>&2 "${PS4:-+}" eval "$@"
-
-	eval "$@"
-}
